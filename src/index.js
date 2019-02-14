2e79e11e711efd4c428785aa2e3e36a38f2cc4ba
Philippe Candille <philippe@oclock.io>
2019-02-14 08:18:07 +0100

    Base React

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Hello');
+/**
+ * NPM import
+ */
+import '@babel/polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ * Local import
+ */
+
+
+/**
+ * Code
+ */
+const Welcome = () => (
+  <div id="welcome">Bienvenue dans React</div>
+);
+
+/**
+ * Render
+ */
+// 1. Le composant racine à rendre (le tronc de l'arbre)
+const rootComponent = <Welcome />;
+// 2. La cible dans le DOM
+const target = document.getElementById('root');
+
+// rendu de react-dom : react VERS dom
+render(rootComponent, target);
